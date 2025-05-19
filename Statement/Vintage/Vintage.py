c35c584496f62483d498733d8067e8a6ce35d951
plutong <LAIBAI@yeah.net>
2025-05-20 00:14:55 +0800

    Update Vintage.py

--- Statement/Vintage/Vintage.py
+++ Statement/Vintage/Vintage.py
@@ -5,21 +5,7 @@ import sys
 sys.path.append(r"D:\Personal\Credit-Risk")
 from common import extract
 
-#%% Vintage Workflow
-class vintage:
-    # Extract data and do primary transformation 
-    def __init__(self, 
-                 debit_path,
-                 plan_path,
-                 snap_date):
-        self.debit_df = extract(debit_path)
-        self.plan_df = extract(plan_path)
-
-        # 由于两类表都是增量数据，选择时间节点提取数据切片
-        self.debit_df = self.debit_df[self.debit_df['snapshot_date'] == snap_date]
-        self.plan_df = self.plan_df[self.plan_df['snapshot_date'] == snap_date]
-        print(f'当前时间节点{snap_date}下的数据量分别为{self.debit_df.shape}和{self.plan_df.shape}')
-
+# %% main 
 if __name__ == '__main__':
     #%% 设置参数
     debit_path = r'D:\Personal\Credit-Risk\Statement\Vintage\repay_actual_data_new.csv'
@@ -58,3 +44,6 @@ if __name__ == '__main__':
     df['balance'] = df['loan_amt'] - df['actual_repay_prin_cumsum']
     df['balance_pre'] = df.groupby('order_id')['balance'].transform(lambda x:x.shift(1))
     # %% 加工逾期标识
+    # 未逾期月份标为0
+    df['overdue_flag'] = 0
+    # 对于
